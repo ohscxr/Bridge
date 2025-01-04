2e0cff74e81ed82475173d8a40709dcb5b12e505
ohscxr <scarishawt@gmail.com>
2025-01-04 03:29:11 -0500

    client side economy data

--- client/client.lua
+++ client/client.lua
@@ -9,26 +9,34 @@ function GetPlayerData()
         data.dob = playerData.dateofbirth
         data.gender = playerData.sex
 
-        local job = {
+        -- todo: new feat - hook into server side ESX.GetJobs() to determine if grade is highest for compatibility with qb-core jobs isboss boolean.
+        data.job = {
             name = playerData.job.name,
             label = playerData.job.label,
             grade = playerData.job.grade,
             gradeName = playerData.job.grade_name,
             onDuty = true, -- todo: look into if ESX has a duty feature, can't remember but iirc it doesn't?
         }
+        
+        -- Player economy data, will probably not reference it much, but could come in use for some edge cases.
+        local accounts = {}
+        for i = 1, #playerData.accounts do
+            local account = playerData[i].accounts
+            accounts[account.name] =  account.money
+        end
 
-        -- todo: new feat - hook into server side ESX.GetJobs() to determine if grade is highest for compatibility with qb-core jobs isboss boolean.
+        data.accounts = accounts
 
-        data.job = job
-    elseif Framework == "QB" or Framework == "QBX" then
-        local playerData = Framework == "QB" and Core.Functions.GetPlayerData() or Framework == "QBX" and exports.qbx_core:GetPlayerData()
+
+    elseif Framework == "QB" then
+        local playerData = Core.Functions.GetPlayerData()
 
         data.identifier = playerData.citizenid
         data.name = string.format("%s %s", playerData.charinfo.firstname, playerData.charinfo.lastname)
         data.dob = playerData.charinfo.birthdate
         data.gender = playerData.charinfo.gender
 
-        local job = {
+        data.job = {
             name = playerData.job.name,
             label = playerData.job.label,
             grade = playerData.job.grade.level,
@@ -37,7 +45,35 @@ function GetPlayerData()
             onDuty = playerData.job.onduty
         }
 
-        data.job = job
+        data.accounts = {
+            money = playerData.money['cash'],
+            bank = playerData.money['bank'],
+            crypto = playerData.money['crypto']
+        }
+
+    elseif Framework == "QBX" then
+        local playerData = exports.qbx_core:GetPlayerData()
+
+        data.identifier = playerData.citizenid
+        data.name = string.format("%s %s", playerData.charinfo.firstname, playerData.charinfo.lastname)
+        data.dob = playerData.charinfo.birthdate
+        data.gender = playerData.charinfo.gender
+
+        data.job = {
+            name = playerData.job.name,
+            label = playerData.job.label,
+            grade = playerData.job.grade.level,
+            gradeName = playerData.job.grade.name,
+            isBoss = playerData.job.isboss,
+            onDuty = playerData.job.onduty
+        }
+
+        data.accounts = {
+            money = playerData.money['cash'],
+            bank = playerData.money['bank'],
+            crypto = playerData.money['crypto']
+        }
+
     else
         -- Other framework bridges
     end
